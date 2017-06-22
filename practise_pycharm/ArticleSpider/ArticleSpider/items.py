b60218656fe5f2db0b4ba1bf124a5613b0ce9242
wuzhenyu02 <wuzy@chinamobilesz.com>
2017-06-22 19:45:12 +0800

    bole article spider and Squish

---
+++ practise_pycharm/ArticleSpider/ArticleSpider/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+"""
+定义数据保存的格式
+"""
+
+import scrapy
+
+
+class ArticlespiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
