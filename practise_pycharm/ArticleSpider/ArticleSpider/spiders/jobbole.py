a941ecbd5d08b4070d32ad89b4ab6887424c84d1
small-cat <hytwzy@163.com>
2017-06-28 23:40:19 +0800

    scrapy to crawl the first page

--- practise_pycharm/ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ practise_pycharm/ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 __author__ = "Sholegance (mblrwuzy@gmail.com)"
 __date__ = ""
@@ -12,7 +13,19 @@ __metaclass__ = type
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/111600/']
 
     def parse(self, response):
+        title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
+        match_re = re.match("([0-9/]*).*", response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip())
+        if match_re:
+            datetime = match_re.group(1)
+        votes = int(response.xpath('//span[contains(@class, "vote-post-up")]/h10/text()').extract()[0])
+        match_fav = re.match('.*(\d+).*', response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0])
+        if match_fav:
+            fav_nums = int(match_fav.group(1))
+
+        print("title:%s, datetime:%s, votes:%d\n" % (title, datetime, votes))
+        print("fav_nums: ", fav_nums)
+
         pass
